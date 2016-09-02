fec8d4b2ad904874edffbcbaaa756afb8238288c
Robert W Boyer <rwboyer@mac.com>
2016-09-02 12:17:52 -0400

    fix content relative to site wide msg

--- js/dist/scripts.js
+++ js/dist/scripts.js
@@ -1780,5 +1780,6 @@ jQuery(document).ready(function( $ ) {
 
 jQuery(document).ready(function(){
   jQuery('header.navigation').css({ top: jQuery('p.demo_store').outerHeight() });
+  jQuery('#content').css({ top: jQuery('p.demo_store').outerHeight() });
 });
 
--- js/src/move-menu.js
+++ js/src/move-menu.js
@@ -1,3 +1,4 @@
 jQuery(document).ready(function(){
   jQuery('header.navigation').css({ top: jQuery('p.demo_store').outerHeight() });
+  jQuery('#content').css({ top: jQuery('p.demo_store').outerHeight() });
 });
